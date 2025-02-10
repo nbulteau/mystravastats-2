62b4f7584fca924abf91dea6bef43582cf093734
Nicolas Bulteau <nicolas.bulteau@orange.com>
2025-02-10 11:37:59 +0100

    fix: add npm install in the build*

--- build-unbuntu.sh
+++ build-unbuntu.sh
@@ -8,7 +8,7 @@ echo "⌛ Building UI project..."
 
 # Build the UI project
 docker run --rm -v "$PWD:/app" -w /app/ui node:latest \
-    sh -c "npm install -g npm@11.1.0 && NODE_OPTIONS='--no-deprecation' npm run build"
+    sh -c "npm install -g npm@11.1.0 2>/dev/null && npm install && VITE_CJS_TRACE=false NODE_OPTIONS='--no-deprecation' npm run build 2>/dev/null"
 
 echo "📦 Copying UI build to back-go/public..."
 # Copy the UI build to the back-go/public directory
