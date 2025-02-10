349bb33a04e5a407bc8b49940ad4dac03633fd0a
Nicolas BULTEAU <nbulteau@gmail.com>
2025-02-10 19:12:39 +0100

    feat: debug mode

--- back-go/main.go
+++ back-go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"flag"
 	"io/fs"
 	"log"
 	"mystravastats/api"
@@ -15,9 +16,13 @@ import (
 var public embed.FS
 
 func main() {
+	// Define a debug flag
+	debug := flag.Bool("debug", false, "run in debug mode")
+	flag.Parse()
+
 	// Create a new CORS handler
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost"}, // Allow any port on localhost
+		AllowedOrigins:   []string{"http://localhost", "http://localhost:5173"}, // Allow any port on localhost and localhost:5173
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
 		AllowedHeaders:   []string{"Content-Type", "Authorization"},
 		AllowCredentials: true,
@@ -26,24 +31,26 @@ func main() {
 	// Create a new router
 	router := api.NewRouter()
 
-	publicFS, err := fs.Sub(public, "public")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Serve static files from the "public" directory with cache headers
-	staticFileHandler := http.FileServer(http.FS(publicFS))
-	cacheControlHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.HasSuffix(r.URL.Path, ".css") {
-			w.Header().Set("Content-Type", "text/css")
-		} else if strings.HasSuffix(r.URL.Path, ".js") {
-			w.Header().Set("Content-Type", "application/javascript")
+	if !*debug {
+		publicFS, err := fs.Sub(public, "public")
+		if err != nil {
+			log.Fatal(err)
 		}
-		w.Header().Set("Cache-Control", "public, max-age=31536000")
 
-		staticFileHandler.ServeHTTP(w, r)
-	})
-	router.PathPrefix("/").Handler(cacheControlHandler)
+		// Serve static files from the "public" directory with cache headers
+		staticFileHandler := http.FileServer(http.FS(publicFS))
+		cacheControlHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if strings.HasSuffix(r.URL.Path, ".css") {
+				w.Header().Set("Content-Type", "text/css")
+			} else if strings.HasSuffix(r.URL.Path, ".js") {
+				w.Header().Set("Content-Type", "application/javascript")
+			}
+			w.Header().Set("Cache-Control", "public, max-age=31536000")
+
+			staticFileHandler.ServeHTTP(w, r)
+		})
+		router.PathPrefix("/").Handler(cacheControlHandler)
+	}
 
 	// Apply the CORS middleware to the router
 	handler := c.Handler(router)
